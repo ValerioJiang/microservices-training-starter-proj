6c3a6cd842befd0b034335cf5097ef61d28d2142
ValerioJiang <jiangvalerio1998@gmail.com>
2024-02-11 17:23:28 +0100

    added register customer

--- customer/src/main/java/com/training/customer/record/CustomerService.java
+++ customer/src/main/java/com/training/customer/record/CustomerService.java
@@ -1,10 +1,11 @@
 package com.training.customer.record;
 
 import com.training.customer.model.Customer;
+import com.training.customer.repository.CustomerRepository;
 import org.springframework.stereotype.Service;
 
 @Service
-public record CustomerService() {
+public record CustomerService(CustomerRepository customerRepository) {
     public void registerCustomer(CustomerRegistrationRequest customerRegistrationRequest) {
 
         Customer customer = Customer.builder()
@@ -12,5 +13,7 @@ public record CustomerService() {
                 .lastName(customerRegistrationRequest.lastName())
                 .email(customerRegistrationRequest.email())
                 .build();
+
+        customerRepository.save(customer);
     }
 }
